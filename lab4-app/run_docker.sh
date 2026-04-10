bcedf7fb2dcb63bb79ecf364142d255ab920e81b
wiktor.rakocki <wiktor.rakocki6@protonmail.com>
2026-04-10 21:03:54 +0200

    exercise lab 4

---
+++ lab4-app/run_docker.sh
@@ -0,0 +1,25 @@
+#Uruchomienie kontenerów
+
+docker stop nginx-proxy api-a api-b 2>/dev/null || true
+docker rm nginx-proxy api-a api-b 2>/dev/null || true
+docker network rm lab3-network 2>/dev/null || true
+
+docker network create lab3-network
+
+docker run -d \
+  --name api-a \
+  -e INSTANCE_ID=api-a \
+  --network lab3-network \
+  rakockiw/lab-app:v1
+
+docker run -d \
+  --name api-b \
+  -e INSTANCE_ID=api-b \
+  --network lab3-network \
+  rakockiw/lab-app:v1
+
+docker run -d \
+  --name nginx-proxy \
+  --network lab3-network \
+  -p 80:80 \
+  rakockiw/lab-nginx:v1
